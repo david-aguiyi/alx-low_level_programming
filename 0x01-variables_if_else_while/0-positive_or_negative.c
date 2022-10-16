74990d3782c8660a67a27886c87b76ad096945f4
david-aguiyi <davidaguiyi9@gmail.com>
2022-10-15 20:42:11 -0700

    Fixed whitspace  bug

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
+#include <stdlib.h>
 
-/* betty style doc for function main goes there */
 /**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Return: Always 0
  */
 
-int main(void){ 
+int main(void)
+{
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
 	if (n > 0)
-		printf("%d is positive\n", n);
+	{
+		printf("%d is positve\n", n);
+	}
 	else if (n == 0)
+	{
 		printf("%d is zero\n", n);
+	}
 	else
+	{
 		printf("%d is negative\n", n);
+	}
+
 	return (0);
 }
