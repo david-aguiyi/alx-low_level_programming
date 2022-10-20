e3ebaf05f2904790d9aae53301ab693b7d39a67a
david-aguiyi <davidaguiyi9@gmail.com>
2022-10-19 20:40:51 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,31 +11,34 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 	{
-		_putchar('0');
-
-		for (mult = 1; mult <= n; mult++)
+		for (num = 0; num <= n; num++)
 		{
-			_putchar(',');
-			_putchar(' ');
+			_putchar('0');
 
-			prod = num * mult;
-
-			if (prod <= 99)
-				_putchr(' ');
-			if (prod >= 100)
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
 				_putchar(' ');
 
-			if (prod >= 100)
-			{
-				_putchar((prod / 100) + '0');
-				_putchar(((prod / 100)) % 10 + '0');
-			}
-			else if (prod <= 99 && prod >= 10)
-			{
+				prod = num * mult;
+
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod >= 100)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 100)) % 10 + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
 				_putchar((prod / 10) + '0');
 			}
 			_putchar((prod / 10) + '0');
 		}
-		_putchar((prod / 10) + '0');
 	}
 }
