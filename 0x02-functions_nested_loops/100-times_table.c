4888a688f04ce865b4734305dadbc12a34c7bd90
david-aguiyi <davidaguiyi9@gmail.com>
2022-10-19 20:45:18 -0700

    hot fix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,13 +24,13 @@ void print_times_table(int n)
 
 				if (prod <= 99)
 					_putchar(' ');
-				if (prod >= 100)
+				if (prod >= 9)
 					_putchar(' ');
 
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + '0');
-					_putchar(((prod / 100)) % 10 + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
 				else if (prod <= 99 && prod >= 10)
 				{
@@ -38,7 +38,7 @@ void print_times_table(int n)
 				}
 				_putchar((prod / 10) + '0');
 			}
-			_putchar((prod / 10) + '0');
+			_putchar('\n');
 		}
 	}
 }
